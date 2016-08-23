7b1b372a7f527b0595a0572f0f2ac1142fe19435
Saiht <mathias_cadet@yahoo.fr>
2016-08-23 11:56:40 +0200

    npm pleeease

---
+++ module13/node_modules/pleeease-cli/CHANGELOG.md
@@ -0,0 +1,59 @@
+4.0.0 - 2015-09-30
+
+* Use [Pleeease 4.0.0](https://github.com/iamvdo/pleeease/releases/tag/4.0.0)
+
+3.2.8 - 2015-05-28
+
+* Fixed: fix Pleeease's version dependency
+
+3.2.7 - 2015-04-27
+
+* Fixed: print compilation errors instead of throwing them
+* Fixed: get only CSS files from directory
+
+3.2.6 - 2015-04-16
+
+* Fixed: watch all files, but ignore non-CSS ones (in case of imports)
+
+3.2.5 - 2015-04-13
+
+* Fixed: watch only inputs files (#2)
+* Fixed: better handle inputs and output files
+* Changed: internal process
+* Added: some tests
+
+3.2.4 - 2015-04-02
+
+* Fixed: use from and to from ClI and copy to sourcemaps
+* Fixed: `-v` returns also version of Pleeease
+
+3.2.3 - 2015-03-10
+
+* Fixed: test sourcemaps.map before use
+
+3.2.2 - 2015-03-10
+
+* Fixed: add deep-extend as a dependency
+
+3.2.1 - 2015-03-03
+
+* Pleeease 3.2.1
+* Fixed:
+  * Write sourcemap file if inline: false
+
+3.2.0 - 2015-02-16
+
+* Match Pleeease 3.2.0
+* Fixed installation problem with Node.js 0.12
+
+3.1.0 - 2015-02-10
+
+* Match Pleeease 3.1.0
+
+3.0.1 - 2015-02-09
+
+* Fixed: Copy `options.out` to `to`
+
+3.0.0 - 2015-02-05
+
+* Initial release (previously included in Pleeease package)
---
+++ module13/node_modules/pleeease-cli/lib/cli.js
@@ -0,0 +1,274 @@
+'use strict';
+
+var fs       = require('fs');
+var path     = require('path');
+
+var globby   = require('globby');
+var extend   = require('deep-extend');
+var mkdirp   = require('mkdirp');
+var chokidar = require('chokidar');
+
+var Pleeease = require('pleeease');
+var Logger   = require('../lib/logger');
+
+/**
+ *
+ * Walk a path and return all files
+ * @param  {String} dir     Directory to walk into
+ * @return {Array}  results Array of files
+ *
+ */
+var walk = function(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+            results = results.concat(walk(file));
+        } else {
+            results.push(file);
+        }
+    });
+    return results;
+};
+
+/**
+ *
+ * Constructor CLI
+ *
+ */
+var CLI = function (inputs, output) {
+
+    inputs = (inputs === undefined || inputs.length === 0) ? undefined : inputs;
+    output = (output === undefined || output === true) ? 'app.min.css' : output;
+
+    var opts = this.extendConfig();
+
+    this.pleeease = new Pleeease.processor(opts);
+
+    inputs = opts.in  || inputs;
+    output = opts.out || output;
+
+    if (this.pleeease.options.sourcemaps) {
+        this.pleeease.options.sourcemaps.to = output;
+    }
+
+    if (inputs === undefined) {
+        return new Logger.error('You must define inputs files');
+    } else {
+        this.files = this.getFiles(inputs, output);
+    }
+
+};
+
+/**
+ *
+ * Extend config with .pleeeaserc
+ *
+ */
+CLI.prototype.extendConfig = function (opts) {
+
+  opts = opts || {};
+
+  // read pleeeaserc
+  var config = {};
+  try {
+    var configFile = '.pleeeaserc';
+    config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+  } finally {
+    return extend(opts, config);
+  }
+
+};
+
+/**
+ *
+ * Get files from glob
+ *
+ */
+CLI.prototype.getFiles = function (inputs, output) {
+
+    var files = globby.sync(inputs);
+
+    if (!files.length) {
+        return new Logger.error('File(s) not found');
+    }
+
+    var results = [];
+
+    // for each file / dir in arguments
+    for (var i = 0; i < files.length; i++) {
+
+        // if it's a directory
+        if (fs.statSync(files[i]).isDirectory()) {
+            // walk path and get all files
+            results = results.concat(walk(files[i]));
+        } else {
+            // it's a file
+            results.push(files[i]);
+        }
+
+    }
+
+    // remove output from inputs
+    if (results.indexOf(output) !== -1) {
+        results.splice(results.indexOf(output), 1);
+    }
+
+    results = results.filter(function (result) {
+        return result.match(/\.(css|scss|sass|less|styl)$/);
+    });
+
+    return {
+        inputs: results,
+        output: output
+    };
+
+};
+
+/**
+ *
+ * Compile files, write processed CSS and map files if asked
+ *
+ */
+CLI.prototype.compile = function (watched) {
+
+    var inputs = this.files.inputs;
+    var output = this.files.output;
+
+    try {
+        this._compile(inputs, output);
+        if (watched) {
+            return new Logger.success('Recompiled file ' + watched);
+        } else {
+            return new Logger.success('Compile ' + inputs.length + ' file(s) [' + inputs + '] to ' + output);
+        }
+    } catch (err) {
+        return new Logger.error('Compilation error\n' + err);
+    }
+
+};
+
+CLI.prototype._compile = function (inputs, output) {
+
+    var cli = this;
+
+    // create a new (future) Root AST
+    var root;
+
+    // get inputs files
+    inputs.map(function(filename) {
+
+        // read, parse and concatenate rules to Root AST
+        var filestring = fs.readFileSync(filename, 'utf-8');
+        if (cli.pleeease.options.sourcemaps) {
+            cli.pleeease.options.sourcemaps.from = filename;
+        }
+        try {
+            var fileAst = cli.pleeease.parse(filestring);
+        } catch (err) {
+            throw new Error(err, filename);
+        }
+
+        // create the final AST
+        if (root === undefined) {
+            root = fileAst;
+        } else {
+            fileAst.each(function(rule) {
+                root.append(rule.clone());
+            });
+        }
+
+    });
+
+    return cli.pleeease.process(root).then(function (fixed) {
+        // create directory if not exists
+        mkdirp.sync(path.dirname(output));
+
+        var sourcemaps = cli.pleeease.options.sourcemaps;
+        if (sourcemaps && sourcemaps.map && sourcemaps.map.inline === false) {
+            fs.writeFileSync(output, fixed.css);
+            fs.writeFileSync(output + '.map', fixed.map);
+        } else {
+            fs.writeFileSync(output, fixed);
+        }
+        return fixed;
+    });
+
+}
+
+/**
+ *
+ * Compile and run watcher
+ *
+ */
+CLI.prototype.watch = function (opts) {
+
+    var cli = this;
+    opts = opts || {};
+
+    // compile first
+    cli.compile();
+
+    return cli.runWatcher(opts);
+
+};
+
+CLI.prototype.runWatcher = function (opts) {
+    var cli = this;
+    var output = this.files.output;
+
+    opts.persistent = (opts.persistent === undefined) ? true : opts.persistent;
+    opts.usePolling = true;
+    opts.ignored = this.ignoredWatch(output);
+    /**
+     *
+     * Watcher
+     * - is `persistent`
+     * - `usePolling` (needed for some text editors)
+     * - ignore files that are not CSS and output file
+     *    (we can't only use inputs because of imported files)
+     */
+    try {
+        var watcher = chokidar.watch(['.'], opts);
+            watcher.on('change', cli.changeDetected.bind(cli));
+        Logger.log('Watcher is running...');
+        return watcher;
+    } catch (err) {
+        throw err;
+    }
+};
+
+CLI.prototype.ignoredWatch = function (output) {
+    return function (path) {
+        if (path === '.')    { return false; }
+        if (path === output) { return true; }
+        return (path.match(/\.(?!(css|scss|sass|less|styl))/));
+    }
+}
+
+CLI.prototype.changeDetected = function (watched) {
+    // when a change is detected, compile files
+    this.compile(watched);
+}
+
+CLI.prototype.closeWatcher = function (watcher) {
+    return watcher.close();
+};
+
+/**
+ *
+ * New CLI instance
+ *
+ */
+var cli = function (inputs, output) {
+    return new CLI(inputs, output);
+};
+
+/**
+ *
+ * Exports
+ *
+ */
+module.exports = cli;
