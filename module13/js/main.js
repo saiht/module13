f9179c89aff7619359395b0832a9d925a422c4a7
Saiht <mathias_cadet@yahoo.fr>
2016-08-24 17:32:55 +0200

    Animated menu footer et placeholder

--- module13/js/main.js
+++ module13/js/main.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {
+
   $('header nav ul li').hover(function() {
     /* Stuff to do when the mouse enters the element */
-    $(this).addClass('animated pulse');
+    $(this).toggleClass('animated pulse');
+  });
+
+  $('footer nav a').hover(function() {
+    /* Stuff to do when the mouse enters the element */
+    $(this).addClass('animated swing');
   }, function() {
     /* Stuff to do when the mouse leaves the element */
-    $(this).removeClass('animated pulse');
+    $(this).removeClass('animated swing');
+  });
+
+  // Ajout/suppression du placeholder
+  $('main form input').focus(function(event) {
+    /* Act on the event */
+    $(this).removeAttr('placeholder');
+  });
+  $('main form input').blur(function(event) {
+    /* Act on the event */
+    $(this).attr('placeholder', 'Que recherchez-vous ?');
   });
 
 
 
 
-  
+
+
 });
